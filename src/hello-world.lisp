8e0a6bdb5a046d486a1febbed42d52de213517f3
Jose Santos <jsmpereira@gmail.com>
2013-02-02 22:36:23 +0000

    Don't hardcode slug directory. jsmpereira/heroku-cl-example#2

--- src/hello-world.lisp
+++ src/hello-world.lisp
@@ -5,6 +5,9 @@
   #+ccl (ccl:getenv target)
   #+sbcl (sb-posix:getenv target))
 
+(defun heroku-slug-dir ()
+  (heroku-getenv "HOME"))
+
 (defun db-params ()
   "Heroku database url format is postgres://username:password@host:port/database_name.
 TODO: cleanup code."
@@ -16,8 +19,8 @@ TODO: cleanup code."
     (list database user password host)))
 
 ;; Handlers
-(push (hunchentoot:create-folder-dispatcher-and-handler "/static/" "/app/public/")
-	 hunchentoot:*dispatch-table*)
+
+(push (hunchentoot:create-folder-dispatcher-and-handler "/static/" (concatenate 'string (heroku-slug-dir) "/public/")) hunchentoot:*dispatch-table*)
 
 (hunchentoot:define-easy-handler (hello-sbcl :uri "/") ()
   (cl-who:with-html-output-to-string (s)
